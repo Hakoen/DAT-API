dfb36076d3c93472250f318ae042d5b9d4d1d41c
Tdallau <tim@dallau.com>
2019-05-28 14:16:59 +0200

    api can now connect with database on server url=http://test.dallau.com:9783; close #4

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import express, { Request, Response } from "express";
+import { Product } from "./Entities";
 // import { User, Tag } from "./entity/User";
 
 const app = express();
@@ -9,14 +10,23 @@ const port = 8080;
 createConnection()
 	.then(async connection => {
 		await connection.synchronize();
+		app.listen(port, () => {
+			// tslint:disable-next-line:no-console
+			console.log(`server is running on: http://localhost:${port}`);
+		});
+
+		app.get("/user", async (req: Request, res: Response) => {
+			let firstUser: Product = null;
+			try {
+				firstUser = await connection
+					.getRepository(Product)
+					.createQueryBuilder("product")
+					.where("product.id = :id", { id: 2 })
+					.getOne();
+			} catch (error) {
+				console.log(error);
+			}
+			res.send(firstUser);
+		});
 	})
 	.catch(error => console.log(error));
-
-app.listen(port, () => {
-	// tslint:disable-next-line:no-console
-	console.log(`server is running on: http://localhost:${port}`);
-});
-
-app.get("/user", (req: Request, res: Response) => {
-	res.send("Working");
-});
