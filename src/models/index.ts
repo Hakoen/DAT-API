971bde82001d820f07a37ff8d89f29af94be6931
Tdallau <tim@dallau.com>
2019-06-11 14:11:48 +0200

    changed user to userTag

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,12 +1,6 @@
 import { ProductCategory } from './productCategoryDbm'
 import { Product } from './productDbm'
 import { Tag } from './tagDbm'
-<<<<<<< HEAD
 import { UserTag } from './userDbm'
 
 export { UserTag, Product, Tag, ProductCategory }
-=======
-import { User } from './userDbm'
-
-export { User, Product, Tag, ProductCategory }
->>>>>>> master
